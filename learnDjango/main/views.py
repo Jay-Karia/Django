30f3393731664ca6c9dd7c9d900f51750e1a22ee
Jay-Karia <jay.sanjay.karia@gmail.com>
2023-09-06 23:03:53 +0530

    till part 3 done

--- learnDjango/main/views.py
+++ learnDjango/main/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import TodoList, Item
 
 # Create your views here.
 def index(request):
     return HttpResponse("hello world")
 
 
-def v1(request):
-    return HttpResponse('this is v1')
+def getList(request, id_):
+    ls = TodoList.objects.get(id=id_)
+    items = ls.item_set.get(id=1)
+    return HttpResponse(f"<h1>{ls}</h1><p>{items}</p>")
